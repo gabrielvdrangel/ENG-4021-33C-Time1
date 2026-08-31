46e43efbf8f25aaaae8d7cdafb6d31bdb73c87eb
Miguel Viana <miguelviana000@gmail.com>
2026-08-31 17:05:04 -0300

    Corrigi pequeno detalhe no bloco principal e escrevi a função de radiciação e porcentagem

--- main.py
+++ main.py
@@ -33,7 +33,8 @@ def operacao_exponenciacao (x,y):
 #RADICIAÇÃO
 
 def operacao_radicacao (x,y):
-    return  
+    resultado = x ** (1/y)
+    return resultado 
 
 
 #DIVISÃO INTEIRA
@@ -51,7 +52,8 @@ def operacao_resto (x,y):
 #PERCENTUAL
 
 def operacao_percentual (x,y):
-    return  
+    resultado = (x/100) * y
+    return resultado
 
 
 #BP
@@ -68,52 +70,46 @@ while True:
 \n8 - Resto
 \n9 - Percentual
 \n0 - Sair do Programa
-\n""")  
-    if escolha == "1":
-        x = float(input("Digite o primeiro número: "))
-        y = float(input("Digite o segundo número: "))
+\n""")
+    x = float(input("Digite o primeiro número: "))
+    y = float(input("Digite o segundo número: "))
+
+    if escolha == "1":    
         resultado = operacao_soma(x,y)
         print(f"\nO resultado da soma é: {resultado}")
+
     elif escolha == "2":
-        x = float(input("Digite o primeiro número: "))
-        y = float(input("Digite o segundo número: "))
         resultado = operacao_subtracao(x,y)
         print(f"O resultado da subtração é: {resultado}")
+
     elif escolha == "3":
-        x = float(input("Digite o primeiro número: "))
-        y = float(input("Digite o segundo número: "))
         resultado = operacao_multiplicacao(x,y)
         print(f"O resultado da multiplicação é: {resultado}")
+
     elif escolha == "4":
-        x = float(input("Digite o primeiro número: "))
-        y = float(input("Digite o segundo número: "))
         resultado = operacao_divisao(x,y)
         print(f"O resultado da divisão é: {resultado}")
+
     elif escolha == "5":
-        x = float(input("Digite a base: "))
-        y = float(input("Digite o expoente: "))
         resultado = operacao_exponenciacao(x,y)
         print(f"O resultado da exponenciação é: {resultado}")
+
     elif escolha == "6":
-        x = float(input("Digite o número: "))
-        y = float(input("Digite o índice da raiz: "))
         resultado = operacao_radicacao(x,y)
         print(f"O resultado da radiciação é: {resultado}")
+
     elif escolha == "7":
-        x = float(input("Digite o dividendo: "))
-        y = float(input("Digite o divisor: "))
         resultado = operacao_divisao_inteira(x,y)
         print(f"O resultado da divisão inteira é: {resultado}")
+
     elif escolha == "8":    
-        x = float(input("Digite o dividendo: "))
-        y = float(input("Digite o divisor: "))
         resultado = operacao_resto(x,y)
         print(f"O resultado do resto da divisão é: {resultado}")
+
     elif escolha == "9":
-        x = float(input("Digite o valor total: "))
-        y = float(input("Digite a porcentagem: "))
         resultado = operacao_percentual(x,y)
         print(f"O resultado do percentual é: {resultado}")
+        
     elif escolha == "0":
         print("Saindo do programa...")
         break
